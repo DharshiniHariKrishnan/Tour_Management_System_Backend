94b68875752b8e49ded7d7ac4f477ac407a9bdd0
Nikhil <njaddu@iu.edu>
2024-04-20 15:12:45 -0400

    d1

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 const allowedOrigins = ['https://main--glowing-biscochitos-6e4f4d.netlify.app/home']; 
 
+
 app.use(cors({
   origin: function (origin, callback) {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
